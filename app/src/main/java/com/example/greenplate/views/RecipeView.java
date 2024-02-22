6ef61cce055752c3d463a0637cd04ab4487bc493
rhea0101 <riasarav@gmail.com>
2024-02-22 15:18:39 -0500

    Recipe Screen with navigation bar

--- app/src/main/java/com/example/greenplate/views/RecipeView.java
+++ app/src/main/java/com/example/greenplate/views/RecipeView.java
@@ -1,4 +1,42 @@
 package com.example.greenplate.views;
 
-public class RecipeView {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.greenplate.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class RecipeView extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.Home) {
+            startActivity(new Intent(RecipeView.this, Home.class));
+            return true;
+        } else if (id == R.id.Recipe) {
+            return true;
+        } else if (id == R.id.InputMeal) {
+            startActivity(new Intent(RecipeView.this, InputMealView.class));
+            return true;
+        } else if (id == R.id.Ingredients) {
+            startActivity(new Intent(RecipeView.this, IngredientsView.class));
+            return true;
+        } else if (id == R.id.ShoppingList) {
+            startActivity(new Intent(RecipeView.this, ShoppingListView.class));
+            return true;
+        }
+        return false;
+    }
 }
