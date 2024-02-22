653b28adc85e142da9183ea3b26c324d65885687
Shriya Buragadda <shriyaburagadda@gmail.com>
2024-02-22 03:18:25 -0500

    Create login screen

--- app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/example/greenplate/viewmodels/LoginViewModel.java
@@ -1,4 +1,63 @@
 package com.example.greenplate.viewmodels;
 
-public class LoginViewModel {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+import android.util.Patterns;
+import android.text.TextUtils;
+import com.example.greenplate.BR;
+
+public class LoginViewModel extends BaseObservable {
+    private User user;
+
+    private String successMessage = "Login successful";
+    private String errorMessage = "Username or Password is not valid";
+
+    @Bindable
+    private String toastMessage = null;
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public void setUserUsername(String username) {
+        user.setUsername(username);
+        notifyPropertyChanged(BR.userUsername);
+    }
+
+    @Bindable
+    public String getUserUsername() {
+        return user.getUsername();
+    }
+
+    @Bindable
+    public String getUserPassword() {
+        return user.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        user.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void onLoginClicked() {
+        if (isInputDataValid()) {
+            setToastMessage(successMessage);
+        } else {
+            setToastMessage(errorMessage);
+        }
+    }
+
+    public boolean isInputDataValid() {
+        return !TextUtils.isEmpty(getUserUsername()) && Patterns.EMAIL_ADDRESS.matcher(getUserUsername()).matches() && getUserPassword().length() > 5; //add more to this logic
+    }
+
 }
--- app/src/main/java/com/example/greenplate/views/LoginView.java
+++ app/src/main/java/com/example/greenplate/views/LoginView.java
@@ -1,4 +1,32 @@
 package com.example.greenplate.views;
 
-public class LoginView {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.greenplate.databinding.LoginScreenBinding;
+import com.example.greenplate.R;
+import com.example.greenplate.viewmodels.LoginViewModel;
+public class LoginView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LoginScreenBinding loginScreenBinding = DataBindingUtil.setContentView(this, R.layout.login_screen);
+        loginScreenBinding.setLoginViewModel(new LoginViewModel());
+
+        loginScreenBinding.executePendingBindings();
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null) {
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
