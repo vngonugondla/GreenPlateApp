afbc3d5f59e7e49d4ef67ff5e5a6cdb31b953d46
vngonugondla <veenagonugondla@gmail.com>
2024-03-25 01:46:04 -0400

    initial changes

--- app/src/main/java/com/example/greenplate/views/RecipeView.java
+++ app/src/main/java/com/example/greenplate/views/RecipeView.java
@@ -5,34 +5,158 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.greenplate.R;
+import com.example.greenplate.model.User;
+import com.example.greenplate.viewmodels.IngredientsViewModel;
+import com.example.greenplate.viewmodels.RecipeViewModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RecipeView extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private EditText ingredientNameEditText;
+    private EditText quantityEditText;
+    private Button addIngredientButton;
+
+    private FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private DatabaseReference root = db.getReference().child("Cookbook");
+    private RecipeViewModel viewModel;
+    private User user = User.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
         Button personalInfoButton = findViewById(R.id.personalInfoButton);
-
-        // Set onClick listener for the button
         personalInfoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Navigate to the personal information screen
                 startActivity(new Intent(RecipeView.this, PersonalInfoView.class));
+            }
+        });
+
+        viewModel = new ViewModelProvider(this).get(RecipeViewModel.class);
+
+        ingredientNameEditText = findViewById(R.id.ingredientNameEditText);
+        quantityEditText = findViewById(R.id.ingredientQuantityEditText);
+        addIngredientButton = findViewById(R.id.addIngredientButton);
+
+        addIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ingredientName = ingredientNameEditText.getText().toString();
+                String quantity = quantityEditText.getText().toString();
+
+                if (ingredientName.isEmpty() || quantity.isEmpty()) {
+                    Toast.makeText(RecipeView.this,
+                            "Please enter both ingredient name and quantity",
+                            Toast.LENGTH_SHORT).show();
+                } else if (Double.parseDouble(quantity) <= 0) {
+                    Toast.makeText(RecipeView.this,
+                            "Quantity must be positive",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    addRecipe(ingredientName, quantity);
+                }
             }
         });
     }
 
+    private void addRecipe(String ingredientName, String quantity) {
+        // Retrieve the username (email) from the User singleton instance
+        String username = user.getUsername();
+        if (username != null && !username.isEmpty()) {
+            // Use only the part before the '@' symbol in the email as the key
+            // and remove any periods or other illegal characters
+            String sanitizedUsername = username.split("@")[0].replaceAll("[.#$\\[\\]]", "");
+
+            // Use the sanitized username to create a reference in your database
+            DatabaseReference userRef = root.child(sanitizedUsername);
+
+            // Check if the ingredient already exists for the user
+            userRef.child(ingredientName).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        // If the ingredient already exists, add the provided quantity to the existing quantity
+                        String existingQuantity = dataSnapshot.getValue(String.class);
+                        double newQuantity = Double.parseDouble(existingQuantity) + Double.parseDouble(quantity);
+                        userRef.child(ingredientName).setValue(String.valueOf(newQuantity))
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(RecipeView.this,
+                                                "Recipe updated in cookbook.",
+                                                Toast.LENGTH_SHORT).show();
+                                        // Navigate to the desired activity after successful update
+                                        Intent intent = new Intent(RecipeView.this, InputMealView.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(RecipeView.this,
+                                                "Failed to update recipe: " + e.getMessage(),
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        userRef.child(ingredientName).setValue(quantity)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void unused) {
+                                        Toast.makeText(RecipeView.this,
+                                                "Recipe added to cookbook.",
+                                                Toast.LENGTH_SHORT).show();
+                                        // Navigate to the desired activity after successful addition
+                                        Intent intent = new Intent(RecipeView.this, InputMealView.class);
+                                        startActivity(intent);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(RecipeView.this,
+                                                "Failed to add recipe: " + e.getMessage(),
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(RecipeView.this,
+                            "Database error: " + databaseError.getMessage(),
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(RecipeView.this,
+                    "Username not set", Toast.LENGTH_SHORT).show();
+        }
+
+    }
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
